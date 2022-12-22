c07d5c0a29facee47d92aba5afa8bc2e8464e39a
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-12-21 23:31:09 -0300

    [NEW] bfs

--- graphzilla/functions/traversal.py
+++ graphzilla/functions/traversal.py
@@ -14,11 +14,11 @@ By default, it uses a iterative approach. A recursive approach is yet to be impl
 It does a preorder traversal in the graph.Inorder and Postorder are yet to be implemented.
 The starting nodes is, by default, node 0. You can change it in the parameter starting_node """
 def depth_first_search_nodes(graph, starting_node: int = 0, mode: str = "preorder"):
-    visited: NDArrayInt = np.zeros(graph.number_of_nodes)
+    visited: NDArrayInt = np.zeros(graph.number_of_nodes, dtype = np.int_)
     stack: deque = deque()
     stack.append(starting_node)
     order_of_visit: list[int] = []
-    while (len(stack) > 0):
+    while len(stack) > 0:
         curr_node: int = stack.pop()
         if not visited[curr_node]:
             visited[curr_node] = 1
@@ -32,11 +32,11 @@ def depth_first_search_nodes(graph, starting_node: int = 0, mode: str = "preorde
 It does a preorder traversal in the graph. Inorder and Postorder are yet to be implemented.
 The starting nodes is, by default, node 0. You can change it in the parameter starting_node """
 def depth_first_search_tree(graph, starting_node: int = 0):
-    visited: NDArrayInt = np.zeros(graph.number_of_nodes)
+    visited: NDArrayInt = np.zeros(graph.number_of_nodes, dtype = np.int_)
     stack: deque = deque()
     stack.append((starting_node, None))
     dfs_tree: Graph = Graph(nodes = graph.nodes)
-    while (len(stack) > 0):
+    while len(stack) > 0:
         curr_node, prev_node = stack.pop()
         if not visited[curr_node]:
             visited[curr_node] = 1
@@ -48,7 +48,25 @@ def depth_first_search_tree(graph, starting_node: int = 0):
     return dfs_tree
 
 """ Performes a breadth first search in a graph, directed or undirected.
+Returns a NxN matrix with the layers oh each node in respect to the starting node (for
+unweighted graphs, it can be used to compute shortest path between starting node and every other node).
 By default, it uses a iterative approach. A recursive approach is yet to be implemented.
 The starting nodes is, by default, node 0. You can change it in the parameter starting_node """
 def breadth_first_search(graph, starting_node: int = 0):
-    pass
+    visited: NDArrayInt = np.zeros(graph.number_of_nodes, dtype = np.int_)
+    layers: NDArrayInt = np.zeros((graph.number_of_nodes, graph.number_of_nodes), dtype = np.int_)
+    stack: deque = deque()
+    stack.append((starting_node,None))
+    while len(stack) > 0:
+        curr_node, prev_node = stack.pop()
+        if prev_node is not None:
+            curr_dist: int = layers[prev_node][curr_node]
+        else:
+            curr_dist: int = 0
+        if not visited[curr_node]:
+            visited[curr_node] = 1
+            for neighboor in graph.neighboors(curr_node):
+                if not visited[neighboor]:
+                    stack.append((neighboor, curr_node))
+                    layers[prev_node][curr_node] = curr_dist + 1
+    return layers
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     nodes = [0,1,2,3,4]
     edges = [(0,1),(0,3),(1,2),(1,3),(1,4),(2,4),(3,4)]
     grafo = Graph(nodes, edges)
-    print(depth_first_search_tree(grafo).adjacency_matrix)
+    print(breadth_first_search(grafo))
 
 if __name__ == "__main__":
     main()
